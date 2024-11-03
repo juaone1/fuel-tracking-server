d33b4bc093de083335096077e8bfae3b41c58e95
juaone1 <johnvirgil06@gmail.com>
2024-11-03 10:07:52 +0800

    added update user admin route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,51 +21,51 @@ const getAllUsers = async (req, res) => {
     }
 
     const { count, rows: users } = await Users.findAndCountAll({
-    attributes: {
-      exclude: ["refreshToken", "deletedAt"],
-    },
-    include: [
-      {
-        model: Offices,
-        as: "office",
-        attributes: ["name"],
+      attributes: {
+        exclude: ["refreshToken", "deletedAt"],
       },
-    ],
+      include: [
+        {
+          model: Offices,
+          as: "office",
+          attributes: ["name"],
+        },
+      ],
       where: whereClause,
       order: [["updatedAt", "DESC"]],
       limit,
       offset,
-  });
+    });
 
     if (!users || users.length === 0) {
-    return res.status(404).json({ error: "Users not found" });
-  }
+      return res.status(404).json({ error: "Users not found" });
+    }
 
-  const transformedUsers = users.map((user) => {
-    const userJSON = user.toJSON();
-    const createdAt = new Date(userJSON.createdAt)
-      .toLocaleString("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      })
-      .replace(/(\d+)\/(\d+)\/(\d+),/, "$1/$2/$3");
+    const transformedUsers = users.map((user) => {
+      const userJSON = user.toJSON();
+      const createdAt = new Date(userJSON.createdAt)
+        .toLocaleString("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        })
+        .replace(/(\d+)\/(\d+)\/(\d+),/, "$1/$2/$3");
 
-    const updatedAt = new Date(userJSON.updatedAt)
-      .toLocaleString("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      })
-      .replace(/(\d+)\/(\d+)\/(\d+),/, "$1/$2/$3");
-    return {
-      ...userJSON,
-      role: userJSON.roleId === 2 ? "Admin" : "User",
-      office: user.office.name,
-      createdAt,
-      updatedAt,
-    };
-  });
+      const updatedAt = new Date(userJSON.updatedAt)
+        .toLocaleString("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        })
+        .replace(/(\d+)\/(\d+)\/(\d+),/, "$1/$2/$3");
+      return {
+        ...userJSON,
+        role: userJSON.roleId === 2 ? "Admin" : "User",
+        office: user.office.name,
+        createdAt,
+        updatedAt,
+      };
+    });
 
     return res.status(200).json({
       totalItems: count,
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,10 @@ const verifyRoles = require("../middlewares/verifyRoles");
 router.get("/", verifyRoles([2]), usersController.getAllUsers);
 router.put("/update-user", usersController.handleUpdateUser);
 router.put("/change-password", usersController.handleChangePassword);
-
+router.put(
+  "/update-user-admin",
+  verifyRoles([2]),
+  usersController.handleUpdateUserAdmin
+);
+router.delete("/:id", verifyRoles([2]), usersController.handleSoftDeleteUser);
 module.exports = router;
