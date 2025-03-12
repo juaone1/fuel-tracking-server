60789dc1ec6a8c771ca0e1abd4d3326dfce0935d
juaone1 <johnvirgil06@gmail.com>
2025-03-12 23:25:20 +0800

    adjust file size to 5mb

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
     if (err.code === "LIMIT_FILE_SIZE") {
       return res
         .status(400)
-        .json({ message: "File is too large. Maximum size is 2MB." });
+        .json({ message: "File is too large. Maximum size is 5MB." });
     }
   }
   if (err) {
